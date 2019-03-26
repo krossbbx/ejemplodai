1d43b5ff62ce9a1ee23755546403824b46cebcf3
Gustavo Guajardo <krossbbx@gmail.com>
2019-03-26 18:20:02 -0300

    segunda parte

--- index.php
+++ index.php
@@ -8,10 +8,15 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        <link href="css/estiloprincipal.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
         <?php
-        // put your code here
+        
         ?>
     </body>
 </html>
